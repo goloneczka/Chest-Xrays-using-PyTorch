3c0aae41d5ddae92a2164fa5c16e757986fa28cd
goloneczka <48791689+goloneczka@users.noreply.github.com>
2021-05-04 15:07:41 +0200

    improve validate set, improve end rule

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from math import fabs
+import shutil
 from random import randrange
 from time import time
 
+import numpy as np
 import torchvision.transforms.functional as TF
 import torch
+from torch.utils.data import random_split
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from CNNModel import CNNModel
 import matplotlib.pyplot as plt
-import torch.nn.functional as F
+
+from CustomImageLoader import CustomImageLoader
 
 DIR = "./data"
-IS_MODEL_ALREADY_TRAINED = True
+IS_MODEL_ALREADY_TRAINED = False
+
+
+def split_data_to_valid_and_train(train_dataset):
+    print("Spliting data into valid / train dataset")
+    torch.manual_seed(42)
+    val_size = int(len(train_dataset) * 0.15)
+
+    return random_split(train_dataset, [len(train_dataset) - val_size, val_size])
 
 
 def data_loader(dir):
@@ -23,33 +34,34 @@ def data_loader(dir):
                                      transforms.ToTensor(),
                                      transforms.Normalize([0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-    valid_tfms = transforms.Compose([
+    test_tfms = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    choosenTransformation = train_tfms if '/train' in dir else valid_tfms
+    choosenTransformation = train_tfms if '/train' or '/val' in dir else test_tfms
     return ImageFolder(dir, transform=choosenTransformation)
 
 
 def calculate_weights(dataset):
     # we are correctly assuming that in ours datasets are only normal( label 0) and pneumonia(label 1) photos
-    # labels_in_dataset = [label for (_, label) in dataset]
-    # count_normal_images = len([label for label in labels_in_dataset if label == 0])
-    # count_pneumonia_images = len([label for label in labels_in_dataset if label == 1])
-    #
-    # return [count_normal_images / len(labels_in_dataset), count_pneumonia_images / len(labels_in_dataset)]
-    return [1500 / 5200, 3500 / 5200]  # -> for faster calculation
+    labels_in_dataset = [label for (_, label) in dataset]
+    count_normal_images = len([label for label in labels_in_dataset if label == 0])
+    print(count_normal_images, ' ', len(labels_in_dataset))
+    weight = [1 / count_normal_images, 1 / (len(labels_in_dataset) - count_normal_images)]
+    return np.array([weight[t] for t in labels_in_dataset])
 
 
 def balance_data_to_learn(dataset):
     print("we are balacing data")
     weights = calculate_weights(dataset)
-    weights = torch.DoubleTensor(weights)
-    sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, 100, replacement=True)
-    return torch.utils.data.DataLoader(dataset, batch_size=28, sampler=sampler, num_workers=1, pin_memory=False)
+    samples_weight = torch.from_numpy(weights)
+    samples_weight = samples_weight.double()
+
+    sampler = torch.utils.data.sampler.WeightedRandomSampler(samples_weight, len(samples_weight))
+    return torch.utils.data.DataLoader(dataset, batch_size=64, sampler=sampler)
 
 
 def loss_plot(avg_loses):
@@ -61,19 +73,27 @@ def loss_plot(avg_loses):
     plt.savefig('loss.png')
 
 
+def stop_criterium(avg_valid_loses, model):
+    if len(avg_valid_loses) <= 3:
+        torch.save(model, './saved_models/{}CNNModel.pt'.format(len(avg_valid_loses)))
+        return False
+    shutil.move("./saved_models/2CNNModel.pt", "./saved_models/1CNNModel.pt")
+    shutil.move("./saved_models/3CNNModel.pt", "./saved_models/2CNNModel.pt")
+    torch.save(model, "./saved_models/3CNNModel.pt")
+    return avg_valid_loses[-3] < avg_valid_loses[-2] < avg_valid_loses[-1]
+
+
 def learning(train_dataset, validation_dataset, model):
     print("we are learning model")
 
     optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.003,
                                 momentum=0.9)  # used in first exp
-    # optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.03, momentum=0.7)     # used in second exp
     criterion = torch.nn.CrossEntropyLoss()
 
     time0 = time()
-    avg_prev_loss = -1
     avg_loses = {"avg_train_loses": [], "avg_valid_loses": []}
 
-    for e in range(25):
+    for e in range(20):
         train_loss = 0
         for images, labels in train_dataset:
             optimizer.zero_grad()
@@ -90,19 +110,20 @@ def learning(train_dataset, validation_dataset, model):
             output = model(images)
             loss = criterion(output, labels)
             valid_loss += loss.item()
-        avg_loses["avg_valid_loses"].append(valid_loss / len(train_dataset))
+        avg_loses["avg_valid_loses"].append(valid_loss / len(validation_dataset))
         print("Epoch {} - Valid loss: {}".format(e, avg_loses["avg_valid_loses"][-1]))
 
-        if fabs(avg_loses["avg_train_loses"][-1] - avg_prev_loss) < 0.001:
+        if stop_criterium(avg_loses["avg_valid_loses"], model):
+            model = torch.load("./saved_models/1CNNModel.pt")
+            model.eval()
             break
-        avg_prev_loss = avg_loses["avg_valid_loses"][-1]
 
     print("\nTraining Time (in minutes) =", (time() - time0) / 60)
     loss_plot(avg_loses)
     return model
 
 
-def get_learned_model(train_dataset, validation_dataset):
+def get_learned_model(train_dataset = None, validation_dataset = None):
     print("we are getting model")
     if IS_MODEL_ALREADY_TRAINED:
         model = torch.load("./CNNModel.pt")
@@ -118,7 +139,6 @@ def get_learned_model(train_dataset, validation_dataset):
 
 def save_wrong_pred(pred, images, labels):
     wrong_idx = (pred != labels.view_as(pred)).nonzero()[:, 0]
-    print(wrong_idx)
     if not len(wrong_idx):
         return
 
@@ -127,13 +147,12 @@ def save_wrong_pred(pred, images, labels):
     actual_pred = labels.view_as(pred)[wrong_idx][-1]
 
     img = TF.to_pil_image(sample)
-    img.save('./wrong_pred/pred{}_actual{}.png'.format(
-        wrong_pred.item(), actual_pred.item()))
+    img.save('./wrong_pred/{}id_pred{}_actual{}.png'.format(
+        randrange(1000), wrong_pred.item(), actual_pred.item()))
 
 
 def check_model(model, val_loader):
     model.eval()
-    test_loss = 0
     correct = 0
     pred_arr, true_arr = [], []
 
@@ -148,19 +167,45 @@ def check_model(model, val_loader):
 
             save_wrong_pred(pred, images, labels)
 
-    print(correct, ' ', len(val_loader.dataset), ' ', test_loss)
+    print(correct, ' ', len(val_loader.dataset), ' ')
     print("\nModel Accuracy =", (100 * correct / len(val_loader.dataset)))
     return pred_arr, true_arr
 
 
+def confusion_matrix(pred_list, true_list):
+    stack = torch.stack((
+        torch.tensor(pred_list),
+        torch.tensor(true_list)
+    ), dim=1)
+
+    cmt = torch.zeros(2, 2, dtype=torch.int64)
+
+    for p in stack:
+        tl, pl = p.tolist()
+        cmt[tl, pl] = cmt[tl, pl] + 1
+
+    return cmt
+
+
 if __name__ == "__main__":
-    train_dataset = data_loader(DIR + '/train')
-    validation_dataset = data_loader(DIR + '/val')
-    test_dataset = data_loader(DIR + '/test')
 
-    train_dataset = balance_data_to_learn(train_dataset)
-    validation_dataset = torch.utils.data.DataLoader(validation_dataset, batch_size=28, shuffle=True)
-    test_dataset = torch.utils.data.DataLoader(test_dataset, batch_size=28, shuffle=True)
+    if not IS_MODEL_ALREADY_TRAINED:
+        train_dataset, validate_dataset = split_data_to_valid_and_train(ImageFolder(DIR + "/train"))
+        validate_dataset = CustomImageLoader(validate_dataset + ImageFolder(DIR + "/val"), " ")
+        train_dataset = CustomImageLoader(train_dataset, "train")
 
-    model = get_learned_model(train_dataset, validation_dataset)
-    pred_arr, true_arr = check_model(model, train_dataset)
+    test_dataset = CustomImageLoader(ImageFolder(DIR + "/test"), " ")
+
+    if not IS_MODEL_ALREADY_TRAINED:
+        train_dataset = balance_data_to_learn(train_dataset)
+        validate_dataset = torch.utils.data.DataLoader(validate_dataset, batch_size=64, shuffle=True)
+    test_dataset = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+    if not IS_MODEL_ALREADY_TRAINED:
+        model = get_learned_model(train_dataset, validate_dataset)
+    else:
+        model = get_learned_model()
+
+    pred_arr, true_arr = check_model(model, test_dataset)
+    print("matrix: ", confusion_matrix(pred_arr, true_arr))
+
